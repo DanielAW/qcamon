30d30e2965236006348b8530cc1c3caa0d23cfaa
Daniel Wegemer <daniel@wegemer.com>
2023-04-23 17:36:29 +0200

    patching c-files works, still needs to fix ugly hack on binutils

--- patches/qca4019/10.4_3.6_00140/hello_world/src/patch.c
+++ patches/qca4019/10.4_3.6_00140/hello_world/src/patch.c
@@ -5,12 +5,19 @@
 #include <wrapper.h>
 #include <helper.h>
 
+/*
 void my_patch(void *a1, void *a2, void *a3, void *a4) {
     asm (
             "mov.n   a10, a2\n"
             "mov.n   a11, a3\n"
             "mov.n   a12, a4\n"
             "mov.n   a13, a5\n"
+
+            //litbase = 0
+            "rsr.litbase a15\n"
+            "movi.n a6, 0\n"
+            "wsr.litbase a6\n"
+            //"rsync\n"
 
             //a6 = 0x1234
             "movi a6, 0x12\n"
@@ -27,10 +34,10 @@ void my_patch(void *a1, void *a2, void *a3, void *a4) {
             "movi a8, 0x88\n"
             "add a7, a7, a8\n"
 
-            //store 0x1234 at 0x9e1300
+            //store 0x1234 at 0x410a88
             "s32i.n  a6, a7, 0\n"
 
-            //a6 = 0xc3834
+            //a6 = 0xc3834 aka wlan_main()
             "movi a6, 0xc\n"
             "slli a6, a6, 16\n"
             "movi a7, 0x38\n"
@@ -38,20 +45,30 @@ void my_patch(void *a1, void *a2, void *a3, void *a4) {
             "add a6, a6, a7\n"
             "movi a7, 0x34\n"
             "add a6, a6, a7\n"
+            //"l32r a6, wlan_main\n"
+
+            //litbase = 0x448001
+            //"movi a7, 0x44\n"
+            //"slli a7, a7, 16\n"
+            //"movi a8, 0x80\n"
+            //"slli a8, a8, 8\n"
+            //"add a7, a7, a8\n"
+            //"movi a7, 0x01\n"
+            //"add a7, a7, a8\n"
+            //"wsr.litbase a7\n"
+            //"rsync\n"
+            "wsr.litbase a15\n"
 
             "callx8 a6\n"
         );
 }
+*/
 
-/*
 void my_patch(void *a1, void *a2, void *a3, void *a4) {
-    int *p = (int *) 0x9e1300;
+    int *p = (int *) 0x410a88;
     *p = 0x1234;
     wlan_main(a1, a2, a3, a4);
 }
-*/
 
-//int32_t wlan_main_ptr = 0xDEADBEEF;
-
-//__attribute__((at(0x409228, "", CHIP_VER_QCA, FW_VER_QCA)))
-//GenericPatch4(wlan_main_ptr_patch, wlan_main_ptr);
+__attribute__((at(0x409228, "", CHIP_VER_QCA, FW_VER_QCA, "segment2_00409200_mod.bin")))
+GenericPatch4(my_patch, my_patch);
