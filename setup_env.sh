1ca8c19d07b2a46828e504c7f3d00e6e10ea0316
Daniel Wegemer <daniel@wegemer.com>
2023-06-04 12:01:14 +0200

    mod setup env

--- setup_env.sh
+++ setup_env.sh
@@ -10,7 +10,7 @@ export PLATFORMUNAME=$(uname -m)
 
 export NEXMON_ROOT=$(pwd)
 
-export CC=xtensa-esp32-elf-
+export CC=$NEXMON_ROOT/buildtools/espressif-esp32-gcc/build-gcc-install/usr/bin/xtensa-esp32-elf-
 export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin/nexmon.so
 export ZLIBFLATE="zlib-flate -compress"
 
