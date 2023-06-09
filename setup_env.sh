a4ffaf8e87ad3b378e98d0dc27ee9a4f9b88702f
Daniel Wegemer <daniel@wegemer.com>
2023-06-09 16:42:27 +0200

    cleaned up setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -2,8 +2,6 @@ OLD_PWD=$(pwd)
 cd $(dirname ${BASH_SOURCE[0]})
 
 export ARCH=xtensa
-#export SUBARCH=arm
-#export KERNEL=kernel7
 
 export HOSTUNAME=$(uname -s)
 export PLATFORMUNAME=$(uname -m)
@@ -12,7 +10,6 @@ export NEXMON_ROOT=$(pwd)
 
 export CC=$NEXMON_ROOT/buildtools/espressif-esp32-gcc/build-gcc-install/usr/bin/xtensa-esp32-elf-
 export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin/nexmon.so
-export ZLIBFLATE="zlib-flate -compress"
 
 export Q=@
 export NEXMON_SETUP_ENV=1
