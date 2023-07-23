fddd4068763e8044b3cb7050bf9ee7aa800b2840
Daniel Wegemer <daniel@wegemer.com>
2023-07-23 13:28:45 +0200

    added call8 alternative to jumpt to wlan_main()

--- patches/qca4019/10.4_3.6_00140/hello_world/src/patch.c
+++ patches/qca4019/10.4_3.6_00140/hello_world/src/patch.c
@@ -13,3 +13,14 @@ void my_patch(void *a1, void *a2, void *a3, void *a4) {
 
 __attribute__((at(0x409228, "", CHIP_VER_QCA, FW_VER_QCA, "segment2_00409200_mod.bin")))
 GenericPatch4(my_patch, my_patch);
+
+//alternative using call8 instead of overwriting literal pool table:
+/*
+//opcode to jump from: 0x9800a0, to: 0x9c8200 => e51548
+__attribute__((at(0x9800a0, "", CHIP_VER_QCA, FW_VER_QCA, "segment2_00980000_mod.bin")))
+GenericPatch1(my_patch1, 0xe5);
+__attribute__((at(0x9800a1, "", CHIP_VER_QCA, FW_VER_QCA, "segment2_00980000_mod.bin")))
+GenericPatch1(my_patch2, 0x15);
+__attribute__((at(0x9800a2, "", CHIP_VER_QCA, FW_VER_QCA, "segment2_00980000_mod.bin")))
+GenericPatch1(my_patch3, 0x48);
+*/
